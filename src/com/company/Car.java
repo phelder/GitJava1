b5a4de8be4ffc2d23815f33c579fff75c3df5261
Helder Pereira <helder.pereira@xing.com>
2020-12-17 13:06:58 +0000

    Added the Car class

---
+++ src/com/company/Car.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}

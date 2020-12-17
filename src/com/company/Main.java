9f87152f1c068b2e610a33b2aa900aa5d4fbfe3f
Helder Pereira <helder.pereira@xing.com>
2020-12-17 13:29:35 +0000

    changed year if my Honda Civic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Person p1 = new Person("Bob", 20, "NY");
         System.out.println(p1);
 
-        Car c1 = new Car("Honda", "Civic", 2000);
+        Car c1 = new Car("Honda", "Civic", 2002);
         System.out.println(c1);
     }
 }

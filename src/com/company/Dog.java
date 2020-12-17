2ca6caab39b4fdc3b9d2c495ebd37922f174c613
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2020-12-17 14:55:41 +0100

    Added new Class - Dog Added new Object form class Dog

---
+++ src/com/company/Dog.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Dog {
+    public String race;
+    public String name;
+    public int age;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,10 @@ public class Main {
 
         Car c1 = new Car("Honda", "Civic", 2000);
         System.out.println(c1);
+
+        Dog dogOne = new Dog();
+        dogOne.name = "Spot";
+        dogOne.race = "Biggle";
+        dogOne.age = 156;
     }
 }
